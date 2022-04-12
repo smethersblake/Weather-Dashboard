3b63b8b4ef367fe19403b9fa34db5f3f195da74e
Blake Smethers <smethersblake@gmail.com>
2022-04-12 18:24:08 -0500

    retrived input text field with city search btn

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        console.log($("#cityInput").val())
+        //var citySearch = $(this).siblings("cityInput").val()
+        //console.log(citySearch.val())
+    })
+});
