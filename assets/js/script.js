e938d962f6f731daba1afb2f57457a7f74d9c6de
Blake Smethers <smethersblake@gmail.com>
2022-04-15 11:56:06 -0500

    layout for 5 day forcast and saved searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,22 +20,20 @@ $(document).ready(function ()
                 var cityLon = data[0].lon
                 var cityLat = data[0].lat
                 apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude={part}&appid=${apiKey}`
-            fetch(apiUrl)
-                .then((response) => response.json())
-                .then((data) =>
-                {
-                    console.log(data)
-                    var daily = data.daily
-                    var current = data.current
-                    console.log(daily)
-                    console.log (current)
-                
-        
-        if ($("#city-current").children().length > 0)
-        {
-            $("#city-current").empty()
-            console.log('yes')
-        }
+                fetch(apiUrl)
+                    .then((response) => response.json())
+                    .then((data) =>
+                    {
+                        console.log(data)
+                        var daily = data.daily
+                        var current = data.current
+                        console.log(daily)
+                        console.log (current)
+                    if ($("#city-current").children().length > 0)
+                    {
+                        $("#city-current").empty()
+                        console.log('yes')
+                    }
                     const searchedCity = $(`<div class="col-12 h-100 bg-warning border border-dark">${city}</div>`)
                     const cityDataUl = $(`<ul style="list-style: none;"></ul>`)
                     const cityDataLiTemp = $(`<li>Temperature: ${current.temp}</li>`)
@@ -46,12 +44,15 @@ $(document).ready(function ()
                     cityDataUl.append(cityDataLiWind)
                     cityDataUl.append(cityDataLiHumidity)
                     cityDataUl.append(cityDataLiUv)
-        searchedCity.append(cityDataUl)
-        $("#city-current").append(searchedCity)
-
-        })
-        })
-        })
+                    searchedCity.append(cityDataUl)
+                    $("#city-current").append(searchedCity)
+                    for (let i = 0; i < daily.length - 3; i++) {
+                        console.log('hello')
+                        
+                    }
+                    })
+            })
+    })
         
         //var citySearch = $(this).siblings("cityInput").val()
         //console.log(citySearch.val())
